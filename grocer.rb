0c472de6c354c426bb58b5db51094cc3f3086de4
Rob Paik <robpaik@gmail.com>
2019-07-22 20:53:35 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -70,10 +70,10 @@ def checkout(cart, coupons)
   cons_cart = consolidate_cart(cart)
   cons_coup_cart = apply_coupons(cons_cart, coupons)
   checkout_cart = apply_clearance(cons_coup_cart)
-  checkout_cart.reduce(nil) do |total, item|
+  checkout_cart.reduce(nil) do |memo, item|
     #binding.pry
     itemprice = item[1][:price] * item[1][:count]
-    total = total + itemprice
+    memo = total + itemprice
     binding.pry
   end
   if total > 100
