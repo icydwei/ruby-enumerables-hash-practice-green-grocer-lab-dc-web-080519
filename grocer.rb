4a6447f3267e073240c05c8ce9c33cbdbd755628
Rob Paik <robpaik@gmail.com>
2019-07-22 16:40:30 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -29,7 +29,10 @@ def apply_coupons(cart, coupons)
             
           end
         else 
-          couponcart[item]
+          couponcart[item.keys[0]] = {}
+          couponcart[item.keys[0]][:price] = item[item.keys[0]][:price]
+          couponcart[item.keys[0]][:clearance] = item[item.keys[0]][:clearance]
+          couponcart[item.keys[0]][:count] = 
         end
       end
   end
