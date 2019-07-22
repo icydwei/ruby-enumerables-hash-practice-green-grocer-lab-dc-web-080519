83c09d49505c53a4bb3c4a525ce7002882e6243c
Rob Paik <robpaik@gmail.com>
2019-07-22 16:19:55 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,20 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
+  final_cart = {}
+  cart.each do |item|
+    #binding.pry
+    if final_cart.include?(item.keys[0])
+      #binding.pry
+      final_cart[item.keys[0]][:count] += 1
+    else
+      final_cart[item.keys[0]] = {}
+      final_cart[item.keys[0]][:price] = item[item.keys[0]][:price]
+      final_cart[item.keys[0]][:clearance] = item[item.keys[0]][:clearance]
+      final_cart[item.keys[0]][:count] = 1
+    end
+  end
+  return final_cart
 end
 
 def apply_coupons(cart, coupons)
